4a8d029eb1b3edb429410aebb594a70f364fa1b6
JEONG99 <jbh990621@gmail.com>
2022-07-24 22:24:48 +0900

    redux-base

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,18 @@
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+import React from 'react';
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default React.memo(CounterContainer);
